f57f0d3db5170002b8d87b57b1a7bcf8871948af
neon1y <romanskiy.aa@gmail.com>
2023-03-27 01:53:59 +0300

    Better apiRequest.js

--- src/apiConfig/apiRequest.js
+++ src/apiConfig/apiRequest.js
@@ -1,35 +1,24 @@
-const API_BASE_URL = 'https://movie-w83k.onrender.com/tmdb/movie';
-const API_KEY = '3b76611b97ee9546a88b0238260eed97';
+export const getPopularMovies = async () => {
+  const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=3b76611b97ee9546a88b0238260eed97&language=en-US&page=1`
+  );
+  const data = await response.json();
+  return data.results;
+};
 
-async function fetchJson(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
-}
+export const getMovies = async () => {
+  const response = await fetch(
+      `https://movie-w83k.onrender.com/movie/page/1`
+  );
+  const data = await response.json();
+  return data;
+};
 
-async function getPopularMovies() {
-  const url = `${API_BASE_URL}/popular?api_key=${API_KEY}&language=en-US&page=1`;
-  const data = await fetchJson(url);
-  return data?.results || [];
-}
 
-async function getMovies(limit = 8) {
-  const url = `${API_BASE_URL}?limit=${limit}`;
-  const data = await fetchJson(url);
-  return data || [];
-}
-
-async function getMovieDetails(id) {
-  const url = `${API_BASE_URL}/${id}?api_key=${API_KEY}`;
-  const data = await fetchJson(url);
-  return data || {};
-}
-
-export { getPopularMovies, getMovies, getMovieDetails };
+export const getMovieDetails = async (id) => {
+  const response = await fetch(
+      `https://movie-w83k.onrender.com/movie/details/${id}`
+  );
+  const data = await response.json();
+  return data;
+};
